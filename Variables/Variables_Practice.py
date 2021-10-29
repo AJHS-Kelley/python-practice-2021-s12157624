08e30dd4b8258ae4c134e92ad521b80c80c1be43
Bruce J <90473269+s12157624@users.noreply.github.com>
2021-10-29 14:42:38 -0400

    0.2
    made is so we can print how many lives we have

--- Variables/Variables_Practice.py
+++ Variables/Variables_Practice.py
@@ -1,4 +1,4 @@
-# Variables Practice, Bruce Johnson, 10/29/21 2:38pm, version 0.1
+# Variables Practice, Bruce Johnson, 10/29/21 2:41pm, version 0.2
 
 # There Common Types of Data
 
@@ -6,3 +6,5 @@ lives_remaining= 30 # INTEGERS, any +/- WHOLE number and 0.
 forty_times = 4.21 # FLOATING POINT, any +/- number with a decimal.
 death_cry = "oh no im dead" 
 # line 7 is a STRING -- collection of letters/numbers/other chars. 
+
+print(lives_remaining) 
