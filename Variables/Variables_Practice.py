f2a1e39637e9c246b4634e1f110ec31e183de283
Bruce J <90473269+s12157624@users.noreply.github.com>
2021-10-29 14:40:18 -0400

    version 0.1
    we added integers, floating, and string

--- Variables/Variables_Practice.py
+++ Variables/Variables_Practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Bruce Johnson, 10/29/21 2:29pm, version 0.0
+# Variables Practice, Bruce Johnson, 10/29/21 2:38pm, version 0.1
+
+# There Common Types of Data
+
+lives_remaining= 30 # INTEGERS, any +/- WHOLE number and 0.
+forty_times = 4.21 # FLOATING POINT, any +/- number with a decimal.
+death_cry = "oh no im dead" 
+# line 7 is a STRING -- collection of letters/numbers/other chars. 
